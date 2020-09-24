91d09b076abbf22d6eaeb093be2bd5b8feba59d6
Jennifer Saratis <jennifer.saratis@gmail.com>
2020-09-24 19:51:50 -0400

    Update server.js
    - res.send in post request

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    res.send('Hello World');
   })
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+    res.send('Hello World');
   });
 
 app.listen(port, () => {
